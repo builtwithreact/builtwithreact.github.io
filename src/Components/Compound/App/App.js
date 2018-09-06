813b72ba6c71e84a19850a412b313eef62362fc9
pankajladhar <ladharpankaj@gmail.com>
2018-09-06 19:48:39 +0530

    Added routing

--- src/Components/Compound/App/App.js
+++ src/Components/Compound/App/App.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route, Link, Switch, BrowserRouter as Router } from 'react-router-dom';
+import Web from './../Web';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-       hello
-      </div>
-    );
-  }
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <Router>
+        <switch>
+          <Route exact path="/" component={Web} />
+          <Route exact path="/web" component={Web} />
+        </switch>
+      </Router>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/Components/Compound/Web/Web.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Web extends Component {
+    render() {
+        return (
+            <div>
+                Web   
+            </div>
+        );
+    }
+}
+
+Web.propTypes = {
+
+};
+
+export default Web;
