29a12503cd58be41cffa87719180ebacc8c02e0e
pankajladhar <ladharpankaj@gmail.com>
2018-09-10 11:56:50 +0530

    Added testdata structure for web

---
+++ src/TestData/WebData.js
@@ -0,0 +1,18 @@
+const WebData = [
+    {
+        "ProjectName": "",
+        "shortDescription": "",
+        "LongDescription": "",
+        "demoUrl": "",
+        "imageUrl": "",
+        "codeRepoUrl": "",
+        "githubUrl": "",
+        "avatarUrl": "",
+        "twitterId": "",
+        "submissionDate": "",
+        "authorName": "",
+        "tags": []
+    }
+]
+
+export default WebData;
