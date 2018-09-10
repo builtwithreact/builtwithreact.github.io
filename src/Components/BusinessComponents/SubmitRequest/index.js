bf63297a2e959775894dbb2835fc3f075dd8f15f
pankajladhar <ladharpankaj@gmail.com>
2018-09-10 12:13:31 +0530

    Added URL for request submisson url

--- src/Components/BusinessComponents/SubmitRequest/index.js
+++ src/Components/BusinessComponents/SubmitRequest/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import InnerSubHeader from './../../BaseComponents/InnerSubHeader'
 import CONSTANTS from './../../../Constants';
+import './SubmitRequest.css';
 
 const SubmitRequest = () => {
     return (
         <div className="SubmitRequest">
             <InnerSubHeader title="Submit Request" />
             <div className="container App__Body SubmitRequest__Body">
-                <iframe src={CONSTANTS.FEEDBACK_FORM_URL}
+                <div className="SubmitRequest__Content">
+                    <h3>
+                        You can submit you request by filling below form.
+                        {/* <br />OR<br />
+                        you can directly write to us.<br />
+                        <a href="mailto:builtwithreact@gmail.com">builtwithreact@gmail.com</a> */}
+                    </h3>
+                </div>
+                <iframe src={CONSTANTS.REQUEST_FORM_URL}
                     title="Submit form"
                     width="100%"
-                    height="580"
+                    height="1300"
                     frameBorder="0"
                     marginHeight="0"
                     marginWidth="0">
--- src/Constants/index.js
+++ src/Constants/index.js
@@ -1,5 +1,6 @@
 const constants = {
     'FEEDBACK_FORM_URL': 'https://docs.google.com/forms/d/e/1FAIpQLScTScfBBGqveOjf4tFctyRpOVmQqzpFtbh8Oj86nYR_1lyeBQ/viewform?embedded=true',
+    'REQUEST_FORM_URL': 'https://docs.google.com/forms/d/e/1FAIpQLSflKhfmWenISdyoiN-jyC_7xxfYEyBGgTI8x8o7gtQgfaZCgg/viewform?embedded=true',
     'NAV_ITEMS': [
         { "title": "Web", "url": "ReactWeb" },
         { "title": "React Native", "url": "ReactNative" },
